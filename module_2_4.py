04cf8158af07b373f2ba5ad5f28607c083bc0501
dreddxyz <99198460+dreddxyz@users.noreply.github.com>
2024-08-30 20:19:44 +0300

    Update module_2_4.py
    Fix bug with 1 appearing in primes

--- module_2_4.py
+++ module_2_4.py
@@ -3,15 +3,17 @@ primes = []
 not_primes = []
 
 for num in numbers:
-  is_prime = True
-  for i in range(2, int(num ** 0.5) + 1):
-      if num % i == 0:
-          is_prime = False
-          break
-  if is_prime:
-      primes.append(num)
-  else:
-      not_primes.append(num)
+    if num == 1:
+        continue
+    is_prime = True
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        primes.append(num)
+    else:
+        not_primes.append(num)
 
 print("Простые:", primes)
 print("Не простые:", not_primes)
