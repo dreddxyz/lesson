519d93951a837b8a0ce64f9c47621aa996d91577
dreddxyz <99198460+dreddxyz@users.noreply.github.com>
2024-09-12 19:11:06 +0300

    Update module_3_3.py
    Tests fix

--- module_3_3.py
+++ module_3_3.py
@@ -2,10 +2,17 @@ def print_params(a = 1, b = 'строка', c = True):
   print(a, b, c)
 
 print_params()
+print_params(2)
+print_params(2, 'тест')
+print_params(3, 'еще_тест', False)
+
+print_params(b = 25)
+print_params(c = [1,2,3])
 
 values_list = [2, 'еще_строка', False]
-values_dict = {'a': 13.37, 'b': 'строкаааа', 'c': [2,2,8]}
 print_params(*values_list)
+
+values_dict = {'a': 13.37, 'b': 'строкаааа', 'c': [2,2,8]}
 print_params(**values_dict)
 
 values_list2 = [1.2, 'опять_строка']
