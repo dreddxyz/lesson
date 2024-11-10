08f63584aa60701fac80e4bcd0599a7116784f44
dreddxyz <99198460+dreddxyz@users.noreply.github.com>
2024-11-10 17:16:19 +0300

    Update module_10_1.py

--- module_10_1/module_10_1.py
+++ module_10_1/module_10_1.py
@@ -1,11 +1,10 @@
 import threading
-from time import sleep, time
+from time import time
 
 def write_words(word_count, file_name):
     with open(file_name, 'w', encoding = 'utf-8') as file:
         for i in range(1, word_count + 1):
             file.write(f"Какое-то слово № {i}\n")
-            sleep(0.1)
     print(f"Завершилась запись в файл {file_name}")
 
 start_time = time()
