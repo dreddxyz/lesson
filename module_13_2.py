d0ee3f25c0150edbcdab1565289d559f6e004ff6
dreddxyz <99198460+dreddxyz@users.noreply.github.com>
2024-12-20 14:03:49 +0300

    Update module_13_2.py

--- module_13_2.py
+++ module_13_2.py
@@ -8,12 +8,10 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    print("Привет! Я бот помогающий твоему здоровью.")
     await message.answer('Привет! Я бот помогающий твоему здоровью.')
 
 @dp.message_handler()
 async def all_messages(message: types.Message):
-    print("Введите команду /start, чтобы начать общение.")
     await message.answer('Введите команду /start, чтобы начать общение.')
 
 if __name__ == "__main__":
